712c1e155625d9455fc6f5880b3a1182d3ece6ca
Pav <PavlikProgrammer@mail.ru>
2014-08-03 16:22:51 +0400

    fst commit

--- app/src/main/java/ru/WinterBall/chatapplication/MainActivity.java
+++ app/src/main/java/ru/WinterBall/chatapplication/MainActivity.java
@@ -15,6 +15,8 @@ public class MainActivity extends Activity {
     }
 
 
+         /*              EvTest             */
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
