8817cc272e2b0ed3256ab6a58ac5408058944567
benzoback <zevzerov@yandex.ru>
2014-08-04 18:10:14 +0400

    реализовал цвета, сделал метод отправки сообщения createMessage(string, type) где type - TYPE_USER (от пользователя) TYPE_SYSTEM (системное)

--- app/src/main/java/ru/WinterBall/chatapplication/MainActivity.java
+++ app/src/main/java/ru/WinterBall/chatapplication/MainActivity.java
@@ -2,10 +2,16 @@ package ru.WinterBall.chatapplication;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.text.Html;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
 import android.text.Spanned;
 import android.text.method.ScrollingMovementMethod;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
@@ -19,18 +25,21 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    TextView chat;
+    TextView chatView;
     EditText message;
     String nickname; //здесь хранится ник
-    int count;
+    int userColor = Color.RED;
+
+    public static final int TYPE_SYSTEM = 0;
+    public static final int TYPE_USER = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        chat = (TextView)findViewById(R.id.textViewChat);
-        chat.setMovementMethod(new ScrollingMovementMethod());
+        chatView = (TextView)findViewById(R.id.textViewChat);
+        chatView.setMovementMethod(new ScrollingMovementMethod());
         message = (EditText)findViewById(R.id.editTextMessage);
 
         askLogin(1337);
@@ -52,15 +61,40 @@ public class MainActivity extends Activity {
 
                 nickname = data.getStringExtra("login");
 
-                chat.setHint("");
-                chat.setGravity(Gravity.NO_GRAVITY);
+                chatView.setHint("");
+                chatView.setGravity(Gravity.NO_GRAVITY);
 
-                Spanned loginLog = Html.fromHtml("<font color=\"red\"> " + nickname + " вошел в чат </font>");
-                chat.append(loginLog);
-                chat.append("\r\n");
+                createMessage("log", TYPE_SYSTEM);
+
             } else {
                 System.exit(0);
             }
+        }
+    }
+
+    protected void createMessage(String msg, int type) {
+
+        if (type == TYPE_SYSTEM) {
+
+            chatView.append("\r\n");
+
+            String userOnline = "system: " + nickname + " вошел в чат :)";
+
+            SpannableStringBuilder log = new SpannableStringBuilder(userOnline);
+            log.setSpan(new ForegroundColorSpan(Color.BLUE), 0, 6, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            log.setSpan(new StyleSpan(Typeface.ITALIC), 6, userOnline.length() - 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            chatView.append(log);
+
+            chatView.append("\r\n");
+        }
+
+        if (type == TYPE_USER) {
+
+            SpannableStringBuilder userPrefix = new SpannableStringBuilder(nickname);
+            userPrefix.setSpan(new ForegroundColorSpan(userColor), 0, nickname.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            chatView.append(userPrefix);
+
+            chatView.append(": " + msg +"\r\n");
         }
     }
 
@@ -98,13 +132,13 @@ public class MainActivity extends Activity {
 
     public void sendButtonClick(View view) {
 
-        chat.setHint("");
-        chat.setGravity(Gravity.NO_GRAVITY);
+        chatView.setHint("");
+        chatView.setGravity(Gravity.NO_GRAVITY);
 
         if (message.getText().toString().replaceAll(" ", "").isEmpty()) {
             message.setHint("need more letters...");
         } else {
-            chat.append(nickname + ": " + message.getText() + "\r\n");
+            createMessage(message.getText().toString(), TYPE_USER);
             message.setHint("");
         }
 
